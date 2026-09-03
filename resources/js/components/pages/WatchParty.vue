59eb51fa17cb2a73951f03a3a57ce840c9e0f122
dyorj <georgespagayanan@gmail.com>
2026-09-04 07:38:47 +0800

    no cookie

--- resources/js/components/pages/WatchParty.vue
+++ resources/js/components/pages/WatchParty.vue
@@ -46,8 +46,8 @@
                         <v-card variant="flat" color="surface-bright" class="border">
                             <v-card-text class="pa-0">
                                 <div v-if="currentVideoId" class="player-wrapper">
-                                    <iframe
-                                        :src="`https://www.youtube.com/embed/${currentVideoId}?autoplay=1&rel=0`"
+                                     <iframe
+                                        :src="`https://www.youtube-nocookie.com/embed/${currentVideoId}?autoplay=1&rel=0`"
                                         title="YouTube video player"
                                         frameborder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
