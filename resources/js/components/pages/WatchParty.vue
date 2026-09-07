544e8bd3fef14d84efe1fc6b6a4dc2d2af9676e0
dyorj <georgespagayanan@gmail.com>
2026-09-07 15:44:23 +0800

    remove splice from queue

--- resources/js/components/pages/WatchParty.vue
+++ resources/js/components/pages/WatchParty.vue
@@ -701,7 +701,7 @@ console.log([1,2,3,4].filter(isEven));`,
             const video = this.queue[index];
             if (video) {
                 this.playVideo(video);
-                this.queue.splice(index, 1);
+                // this.queue.splice(index, 1);
             }
         },
 
