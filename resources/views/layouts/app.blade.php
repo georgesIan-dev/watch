358253a872defc1a54572ab0d6b89b7e8d7a50d1
dyorj <georgespagayanan@gmail.com>
2026-09-18 07:47:10 +0800

    add icon and title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-    <title>Monitoring System</title> --}}
+    <title>Monitoring System</title>
+    <link rel="icon" type="image/png" href="/pwa-192x192.png">
     <link rel="manifest" href="/manifest.webmanifest">
     <meta name="theme-color" content="#000000">
 
